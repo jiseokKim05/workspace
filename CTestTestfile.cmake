0c052478021e950a835a8b3874e5db3c13caa3d9
Ji Seok KIM <159974106+jiseokKim05@users.noreply.github.com>
2025-02-07 20:27:36 +0900

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amap/aMAP_catkin_ws/src
-# Build directory: /home/amap/aMAP_catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("opencv_line_detection")
-subdirs("sc_mini")
